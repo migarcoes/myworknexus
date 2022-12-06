c438dc08f3b876c8f656cf2ee1d0bd5a1526217a
Glitch (glitch-blank-sqlite) <none>
2022-12-06 12:33:28 +0000

    🏃🛬 Checkpoint ./server.js:31317407/72 ./package.json:31317407/1137

--- server.js
+++ server.js
@@ -86,9 +86,8 @@ const authorized = key => {
 // Run the server and report out to the logs
 fastify.listen({port:process.env.PORT, host:'0.0.0.0'}, function(err, address) {
   if (err) {
-    fastify.log.error(err);
+    console.error(err);
     process.exit(1);
   }
   console.log(`Your app is listening on ${address}`);
-  fastify.log.info(`server listening on ${address}`);
 });
